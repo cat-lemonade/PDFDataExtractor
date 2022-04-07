5f21a138a82fd23bf932b0efcbe8c3003eacdb20
Miao Zhu <mz397@cam.ac.uk>
2022-04-07 15:28:00 +0100

    add chem extraction

--- pdfdataextractor/demo.py
+++ pdfdataextractor/demo.py
@@ -1,39 +1,40 @@
 from pdfdataextractor import Reader
 import glob
 
+
 def read_single(file):
     reader = Reader()
     pdf = reader.read_file(file)
+
     try:
+        # print(pdf.plaintext())
+        # print(pdf.test())
+
+        a = pdf.abstract(chem=True)
+        print(a.records.serialize())
+
+        # print(pdf.title())
+        # print(pdf.doi())
+        # print(pdf.caption())
+        # print(pdf.author())
+        # print(pdf.journal())
         # print(pdf.keywords())
-        for i,j in pdf.reference().items():
-            print(i)
-            print(j)
 
-
-        # from pdfminer.pdfparser import PDFParser
-        # from pdfminer.pdfdocument import PDFDocument
-        #
-        # fp = open(file, 'rb')
-        # parser = PDFParser(fp)
-        # doc = PDFDocument(parser)
-        #
-        # print(doc.info)  # The "Info" metadata
+        # print(pdf.section())
+        # for i,j in pdf.reference().items():
+        #     print(i)
+        #     print(j)
 
 
     except:
         pass
 
+
 def read_multiple(path):
     for seq, pdf in enumerate(path):
-        print(seq)
         read_single(pdf)
         print('-------------------', '\n')
 
-
-
-
-
-read_multiple(glob.glob(r'/Users/miao/Desktop/PDFDataExtractor/SI/Others/acs/*.pdf'))
-
-# read_single(r'/Users/miao/Desktop/PDFDataExtractor/SI/Others/acs/acs.est.0c08390.pdf')
+# read_multiple(glob.glob(r'/Users/miao/Desktop/PDFDataExtractor/SI/Others/acs/*.pdf'))
+# read_single(r'/Users/miao/Documents/untitled folder/articles/Elesvier/battery/-Green--effects-of-hybrid-actors-through-carbon-tr_2020_Global-Transitions-P.pdf')
+# read_single(r'/Volumes/Backup/PDE_papers/articles/Elesvier/dssc/The-effect-of-molecular-structure-on-the-properties-of-quinox_2020_Dyes-and-.pdf')
